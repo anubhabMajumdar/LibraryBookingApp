51112d576816e42de13b6ea56f5d3743836afc13
Anubhab Majumdar <anubhabmajumdar93@gmail.com>
2016-09-11 23:01:43 -0400

    Signup page working

--- Project1/app/controllers/users_controller.rb
+++ Project1/app/controllers/users_controller.rb
@@ -5,5 +5,25 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+
+  def create
+    @user = User.new(allowed_params)
+    if @user.save
+      flash[:success] = ("Registration successful " + @user.name)
+      redirect_to user_url(@user)
+    else
+      render 'users/new'
+    end
+  end
+
+  private
+
+    def allowed_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+
 end
