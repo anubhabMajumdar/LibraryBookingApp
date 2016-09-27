bd1897bf4c2660d15d979db57b4d007ed343f421
Lei Zhang <lzhang45@ncsu.edu>
2016-09-27 10:03:49 -0400

    Some debug work by Lei Zhang

--- Project1/app/controllers/bookings_controller.rb
+++ Project1/app/controllers/bookings_controller.rb
@@ -119,43 +119,72 @@ end
   def create
 
     @booking = Booking.new(booking_params)
-     @booking.username = User.find(session[:user_id]).email
+     @booking.name = User.find(session[:user_id]).email
+     #name is in fact the email of the person who books the room (By Lei Zhang)
      @booking.bookday=Time.new
        #respond_to do |format|
-    if booking_params["date(2i)"].length==1
-      bookdate=booking_params["date(1i)"]+"-0"+booking_params["date(2i)"]+"-"+booking_params["date(3i)"]+" 00:00:00"
-    else
-      bookdate=booking_params["date(1i)"]+"-"+booking_params["date(2i)"]+"-"+booking_params["date(3i)"]+" 00:00:00"
-      end
-    @booking.endtime = @booking.endtime+":00"
-    @booking.starttime = @booking.starttime+":00"
+    #if booking_params["date(2i)"].length==1
+    #  bookdate=booking_params["date(1i)"]+"-0"+booking_params["date(2i)"]+"-"+booking_params["date(3i)"]+" 00:00:00"
+    #else
+    #  bookdate=booking_params["date(1i)"]+"-"+booking_params["date(2i)"]+"-"+booking_params["date(3i)"]+" 00:00:00"
+    #end
 
-    @bookingrecord=Booking.where("room_id= ? and date = ?",booking_params[:room_id],bookdate)
-      if (Time.parse(@booking.endtime)-Time.parse(@booking.starttime))/1800 > 4 ||Time.parse(@booking.endtime)-Time.parse(@booking.starttime)<0
-        flash[:danger] = "Cannot book for more that 2 hours"
-        redirect_to bookings_path
-      elsif not (timeconstrain(@bookingrecord,@booking))
-        flash[:danger] = "The room is booked during that period. Try another room or another time"
+    #-------------
+    #Cannot undertand what the following two lines of code mean (Lei Zhang)
+    #@booking.endtime = @bookdate.endtime+":00"
+    #@booking.starttime = @bookdate.starttime+":00"
+    #-------------
+
+
+    #--------------
+    #<begin> edit by Lei Zhang
+    starttime_string = booking_params[:starttime]
+    if starttime_string.length == 4
+        starttime_string = "0" + starttime_string
+    end
+    endtime_string = booking_params[:endtime]
+    if endtime_string.length == 4
+        endtime_string = "0" + endtime_string
+    end
+
+    @booking.endtime = Time.parse("%04d-%02d-%02d %s:00" %[booking_params["date(1i)"], booking_params["date(2i)"], booking_params["date(3i)"], endtime_string])
+    @booking.starttime = Time.parse("%04d-%02d-%02d %s:00" %[booking_params["date(1i)"], booking_params["date(2i)"], booking_params["date(3i)"], starttime_string])
+    #<end> edited by Lei Zhang
+    #--------------
+    
+    @bookingrecord=Booking.where("room_id= ? and date = ?",booking_params[:room_id],@booking.date)
+
+
+
+    #-------------
+    #<begin> edited by Lei Zhang
+    debugger
+    duration = @booking.endtime - @booking.starttime
+    if ((duration/1800 > 4) || (duration<0))
+      flash[:danger] = "Cannot book for more that 2 hours or less than 0 hours"
+      redirect_to bookings_path
+    elsif not (timeconstrain(@bookingrecord,@booking))
+      flash[:danger] = "The room is booked during that period. Try another room or another time"
+      redirect_to bookings_path
+    else
+      # respond_to do |format|
+      #    if @booking.save
+      #     format.html { redirect_to @booking, notice: 'Booking was successfully created.' }
+      #     format.json { render :show, status: :created, location: @booking }
+      #   else
+      #     format.html { render :new }
+      #     format.json { render json: @booking.errors, status: :unprocessable_entity }
+      #    end
+      # end
+      if @booking.save
+        flash[:success] = "Room successfully booked"
+      # redirect_to bookings_path
         redirect_to bookings_path
       else
-        # respond_to do |format|
-        #    if @booking.save
-        #     format.html { redirect_to @booking, notice: 'Booking was successfully created.' }
-        #     format.json { render :show, status: :created, location: @booking }
-        #   else
-        #     format.html { render :new }
-        #     format.json { render json: @booking.errors, status: :unprocessable_entity }
-        #    end
-        # end
-        if @booking.save
-          flash[:success] = "Room successfully booked"
-        # redirect_to bookings_path
-          redirect_to bookings_path
-        else
-          flash[:danger] = "Cannot book room now. Please try later"
-          redirect_to bookings_path
-        end
-        end
+        flash[:danger] = "Cannot book room now. Please try again later"
+        redirect_to bookings_path
+      end
+    end
  end
 
   # PATCH/PUT /bookings/1
@@ -201,15 +230,15 @@ end
     def timeconstrain(bookingrecord,booking )
         timeslot=Array.new(48,0)
         bookingrecord.each do |book|
-        startindex=(Time.parse(book.starttime).hour)*2+(Time.parse(book.starttime).min)/30
-        endindex=(Time.parse(book.endtime).hour)*2+(Time.parse(book.endtime).min)/30
+        startindex=(book.starttime.hour)*2+(book.starttime.min)/30
+        endindex=(book.endtime.hour)*2+(book.endtime.min)/30
         while startindex<endindex
           timeslot[startindex]=1
           startindex=startindex+1
         end
         end
-        insertstart=(Time.parse(booking.starttime).hour)*2+(Time.parse(booking.starttime).min)/30
-        insertend=(Time.parse(booking.endtime).hour)*2+(Time.parse(booking.endtime).min)/30
+        insertstart=(booking.starttime.hour*2+booking.starttime.min)/30
+        insertend=(booking.endtime.hour)*2+(booking.endtime.min)/30
         while insertstart<insertend
               if timeslot[insertstart]!=0
                 return false
